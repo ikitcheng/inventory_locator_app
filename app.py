a5ef876a26b9276f6e035f778bd0ba7c79779893
I Kit Cheng <matthewkit@gmail.com>
2023-01-27 22:09:25 +0000

    [FEATURE] allow user to enter multiple item names separated by a space or comma

--- app.py
+++ app.py
@@ -13,18 +13,30 @@ st.set_page_config(page_title="Inventory Locator App", page_icon=":guardsman:", 
 # Create a dictionary to store the inventory
 inventory = json.load(open('./data/inventory.json'))
 
-def search_item(item_name):
+def search_item(item_names):
     cols = st.columns(2)
-    if item_name in inventory:
-        item = inventory[item_name]
-        locations = list(zip(item['zone'], item['shelf']))
-        maps = preprocess_map(locations)
-        cols[0].success(f"Item found: {item_name}")
-        for l in locations:
-            cols[0].write(f"Location: Zone {l[0]}, Shelf {l[1]}")
-        display_map(maps)
+
+    # Strip the item name and split it if there are multiple items
+    item_names = item_names.strip()
+    if ' ' in item_names:
+        item_names = item_names.split(' ')
+    elif ',' in item_names:
+        item_names = item_names.split(',')
     else:
-        cols[0].error(f"Item not found: {item_name}")
+        item_names = [item_names]
+    
+    # Loop through the item names and show the location and map
+    for item_name in item_names:
+        if item_name in inventory:
+            item = inventory[item_name]
+            locations = list(zip(item['zone'], item['shelf']))
+            maps = preprocess_map(locations)
+            cols[0].success(f"Item found: {item_name}")
+            for l in locations:
+                cols[0].write(f"Location: Zone {l[0]}, Shelf {l[1]}")
+            display_map(maps, cols[0])
+        else:
+            cols[0].error(f"Item not found: {item_name}")
 
 
 def preprocess_map(locations):
@@ -40,15 +52,15 @@ def preprocess_map(locations):
             continue
     return maps
 
-def display_map(maps):
+def display_map(maps, col):
     map_final = np.sum(maps, axis=0)
-    st.image(map_final, caption='Your items are in the highlighted zones.', clamp=True)
+    col.image(map_final, caption='Your items are in the highlighted zones.', clamp=True)
 
 def main():
     st.title("Inventory Locator App")
     st.write("Search for an item in the inventory:")
     cols = st.columns(2)
-    item_name = cols[0].text_input("Item name:")
+    item_name = cols[0].text_input("Item name(s):")
     if st.button("Search"):
         search_item(item_name.upper())
 
